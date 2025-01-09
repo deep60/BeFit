d74f15456d368957ee91d734040e7a195dd3e1c5
deep60 <deep6060p@gmail.com>
2025-01-09 12:26:04 +0530

    Initial Commit

---
+++ BeFit/BeFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeFitApp.swift
+//  BeFit
+//
+//  Created by P Deepanshu on 09/01/25.
+//
+
+import SwiftUI
+
+@main
+struct BeFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
